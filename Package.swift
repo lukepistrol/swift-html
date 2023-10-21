8e76176bbe7b0e4f743b85ad920fee17e95674ef
Lukas Pistrol <lukas@pistrol.com>
2023-10-21 12:54:21 +0200

    disable snapshot testing

--- Package.swift
+++ Package.swift
@@ -12,34 +12,10 @@ let package = Package(
     .library(
       name: "Html",
       targets: ["Html"]),
-    .library(
-      name: "HtmlSnapshotTesting",
-      targets: ["HtmlSnapshotTesting"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.11.0")
   ],
   targets: [
     .target(
       name: "Html"
-    ),
-    .testTarget(
-      name: "HtmlTests",
-      dependencies: ["Html"]
-    ),
-    .target(
-      name: "HtmlSnapshotTesting",
-      dependencies: [
-        "Html",
-        .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
-      ]
-    ),
-    .testTarget(
-      name: "HtmlSnapshotTestingTests",
-      dependencies: ["HtmlSnapshotTesting"],
-      exclude: [
-        "__Snapshots__"
-      ]
     ),
   ]
 )
